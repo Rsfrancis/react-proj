4e25abe1b426436da197af7a65ba896deb081e2b
Rsfrancis <arturfdamas2@gmail.com>
2025-08-22 00:05:52 +0000

    commit

--- atividade/src/components/profile.js
+++ atividade/src/components/profile.js
@@ -5,7 +5,9 @@ export default function Profile() {
     <div>
       <Image src="/eu.jpeg" alt="Minha Foto" width={150} height={150} />
       <h2>Artur Francisco Damascena</h2>
-      <p>Sou estudante de programação e estou aprendendo Front-End. tenho 19 anos e gosto de escutar musicas!</p>
+      <p>Sou estudante de programação e estou aprendendo Front-End.</p>
+      <p>Tenho 19 anos e gosto de escutar musicas!</p>
+      <p>e sou estudante da catolica!</p>
     </div>
   );
 }
--- atividade/src/pages/index.js
+++ atividade/src/pages/index.js
@@ -3,7 +3,7 @@ import Profile from '@/components/profile';
 export default function Home() {
   return (
     <div>
-      <h1></h1>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
       <Profile />
     </div>
   );
